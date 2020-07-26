efd1ccfa61b05ef0de1e578bc4a50c9263e05a28
gabyzif <gabyzifferman@MacBook-Pro-de-Gaby.local>
2020-07-26 03:57:20 -0300

    do the thing

---
+++ script.js
@@ -0,0 +1,95 @@
+/* The dragging code for '.draggable' from the demo above
+ * applies to this demo as well so it doesn't have to be repeated. */
+
+// enable draggables to be dropped into this
+window.onload = function () {
+  document.getElementById("button").onclick = function () {
+    document.getElementById("modal").style.display = "none";
+  };
+};
+
+function dragMoveListener(event) {
+  var target = event.target,
+    // keep the dragged position in the data-x/data-y attributes
+    x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx,
+    y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.webkitTransform = target.style.transform =
+    "translate(" + x + "px, " + y + "px)";
+
+  // update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
+
+// target elements with the "draggable" class
+interact(".draggable").draggable({
+  // enable inertial throwing
+  inertia: true,
+  // keep the element within the area of it's parent
+  modifiers: [
+    interact.modifiers.restrictRect({
+      restriction: "parent",
+      endOnly: true,
+    }),
+  ],
+  // enable autoScroll
+  autoScroll: true,
+
+  listeners: {
+    // call this function on every dragmove event
+    move: dragMoveListener,
+
+    // call this function on every dragend event
+    end(event) {
+      var textEl = event.target.querySelector("p");
+
+      textEl &&
+        (textEl.textContent =
+          "moved a distance of " +
+          Math.sqrt(
+            (Math.pow(event.pageX - event.x0, 2) +
+              Math.pow(event.pageY - event.y0, 2)) |
+              0
+          ).toFixed(2) +
+          "px");
+    },
+  },
+});
+
+function dragMoveListener(event) {
+  var target = event.target;
+  // keep the dragged position in the data-x/data-y attributes
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
+
+  // translate the element
+  target.style.webkitTransform = target.style.transform =
+    "translate(" + x + "px, " + y + "px)";
+
+  // update the posiion attributes
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
+}
+
+// this function is used later in the resizing and gesture demos
+window.dragMoveListener = dragMoveListener;
+
+/// IMAGES
+var maxPages = 27;
+
+var allImagesContainerElement = document.getElementById("allImagesContainer");
+
+var arr = [];
+
+for (var i = 1; i <= maxPages; i++) {
+  var element = document.createElement("img");
+  element.src = `img/ro-${i}.png`;
+  arr[i] = element.src;
+  element.className = "draggable";
+  element.setAttribute("style", ` z-index: ${i}; `);
+  allImagesContainerElement.appendChild(element);
+}
+
+//
